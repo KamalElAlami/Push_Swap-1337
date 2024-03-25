fc2574dae7b1af99a58e193ac274e5cf3912a070
Kamal El Alami <kelalami123@gmail.com>
2024-03-25 23:13:38 +0000

    structure the program

--- args_parsing.c
+++ args_parsing.c
@@ -1,24 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   args_parsing.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/25 22:11:37 by marvin            #+#    #+#             */
-/*   Updated: 2024/03/25 22:11:37 by marvin           ###   ########.fr       */
+/*   Created: 2024/03/25 23:10:07 by marvin            #+#    #+#             */
+/*   Updated: 2024/03/25 23:10:07 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 #include "./includes/push_swap.h"
 
-
-
-int main(int ac, char **av)
+void check_overflow(char **args)
 {
-    if (ac == 1)
-        return (1);
-    check_args(av);    
-     
+
+}
+
+void check_validity(char **args)
+{
+    
+}
+
+void check_spaces(char **args)
+{
+
+}
+
+void    check_args(char **args)
+{
+    
 }
